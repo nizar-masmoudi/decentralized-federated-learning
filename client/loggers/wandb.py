8545b2ff33cf839b3558ec87adca83489a9e4a58
nizar-masmoudi <nizarmasmoudi@outlook.fr>
2023-09-01 20:53:12 +0100

    Add dist matrix + round index

--- client/loggers/wandb.py
+++ client/loggers/wandb.py
@@ -1,9 +1,11 @@
-from typing import Callable
+from typing import Callable, Sequence
 
 import torch
 import torch.nn as nn
 import wandb
 from dotenv import load_dotenv
+from itertools import permutations
+import pandas as pd
 
 load_dotenv()
 
@@ -16,12 +18,12 @@ class WandbLogger:
         self.config = config
         self.run = wandb.init(project=self.project, name=self.name, group=self.group, config=self.config)
         # Defining metrics
-        self.run.define_metric('epoch')
-        self.run.define_metric('tloss', step_metric='epoch', summary='min', goal='minimize')
-        self.run.define_metric('vloss', step_metric='epoch', summary='min', goal='minimize')
+        # self.run.define_metric('epoch')
+        # self.run.define_metric('tloss', step_metric='epoch', summary='min', goal='minimize')
+        # self.run.define_metric('vloss', step_metric='epoch', summary='min', goal='minimize')
 
-    def log_metrics(self, metric_dict: dict, epoch: int):
-        self.run.log({**metric_dict, 'epoch': epoch})
+    def log_metrics(self, metric_dict: dict, epoch: int, ridx: int):
+        self.run.log({**metric_dict, 'epoch': epoch, 'round': ridx})
 
     def watch(self, model: nn.Module, loss_fn: Callable, client_id: int):
         self.run.watch(model, loss_fn, log='all', idx=client_id)
@@ -31,3 +33,14 @@ class WandbLogger:
 
     def confusion_matrix(self, ground_truth: torch.Tensor, prediction: torch.Tensor):
         self.run.log({'cm': wandb.plot.confusion_matrix(y_true=ground_truth, probs=prediction)})
+
+    def distance_matrix(self, clients: Sequence, distance_fn: Callable):
+        dists = [[.0 for _ in range(len(clients))] for _ in range(len(clients))]
+        for ci, cj in permutations(clients, 2):
+            dists[clients.index(ci)][clients.index(cj)] = distance_fn(ci, cj)
+        dist_matrix = pd.DataFrame(dists, columns=[str(client) for client in clients]).round(3)
+        dist_matrix.insert(loc=0, column='', value=pd.Series(f'**{client}**' for client in clients))
+        self.run.log({'dists': wandb.Table(dataframe=dist_matrix)})
+
+    def finish(self):
+        self.run.finish()
