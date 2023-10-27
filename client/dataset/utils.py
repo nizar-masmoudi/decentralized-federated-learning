c19ea19640b0633701d197ff9c20d0664f52fbf6
nizar-masmoudi <nizarmasmoudi@outlook.fr>
2023-10-27 15:15:31 +0100

    Fix train_valid_split + Add pytest

--- client/dataset/utils.py
+++ client/dataset/utils.py
@@ -1,13 +1,13 @@
 from torch.utils.data import Dataset, random_split
 
 
-def train_valid_split(dataset: Dataset, valid_split: float = .1):
+def train_valid_split(dataset: Dataset, validation_split: float = .1):
     """
     Split dataset into train and vlidation splits.
     :param dataset: Dataset to split.
-    :param valid_split: Validation split
+    :param validation_split: Validation split
     :return: Train and validation splits
     """
-    valid_split = int((1 - valid_split) * len(dataset))
-    train_split = len(dataset) - valid_split
-    return random_split(dataset, [train_split, valid_split])
+    validation_size = int(validation_split * len(dataset))
+    train_size = len(dataset) - validation_size
+    return random_split(dataset, [train_size, validation_size])
