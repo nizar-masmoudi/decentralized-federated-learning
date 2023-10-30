4ed6eaad9ac0e4cf1aa7d73756a9e4bb908ea771
nizar-masmoudi <nizarmasmoudi@outlook.fr>
2023-10-30 10:02:29 +0100

    Cast layer tensor as floats

--- client/aggregation/averaging.py
+++ client/aggregation/averaging.py
@@ -12,5 +12,5 @@ class FedAvg(Aggregator):
     def aggregate(self, state_dicts: List[Dict]) -> Dict:
         agg_state = state_dicts[0].copy()
         for key in agg_state:
-            agg_state[key] = torch.stack([state[key] for state in state_dicts if key in state.keys()]).mean(0)
+            agg_state[key] = torch.stack([state[key] for state in state_dicts if key in state.keys()]).float().mean(0)
         return agg_state
